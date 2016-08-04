622b3819d5bd67c109cc64e13e09c5b25af2e6b1
ingvar73 <ingvar73@gmail.com>
2016-08-05 01:48:31 +0300

    Условия

--- homework4.php
+++ homework4.php
@@ -5,12 +5,12 @@
  * Date: 05.08.2016
  * Time: 1:19
  */
-    $age = -143;
+    $age = 130;
     if ($age >= 18 and $age <= 66)
             echo "Вам еще работать и работать!";
         elseif ($age >= 1 and $age <= 17)
             echo "Вам еще рано работать";
-        elseif ($age > 65)
+        elseif ($age > 65 and $age < 120)
             echo "Вам пора на пенсию";
         else
             echo "Неизвестный возраст!";
