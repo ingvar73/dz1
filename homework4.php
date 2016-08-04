e6d66e52c37d008a691b20665001be9aa84e9eb1
ingvar73 <ingvar73@gmail.com>
2016-08-05 01:38:37 +0300

    Условия

---
+++ homework4.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ingvar73
+ * Date: 05.08.2016
+ * Time: 1:19
+ */
+    $age = 143;
+    if ($age < 19)
+            echo "Вам еще рано работать";
+        elseif ($age < 66)
+            echo "Вам еще работать и работать!";
+        elseif ($age > 66)
+            echo "Вам пора на пенсию";
+//        else
+//            echo "Неизвестный возраст!";
