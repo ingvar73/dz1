4b275fd8d825a9a8ea0b7943ef06cde32fd59d29
ingvar73 <ingvar73@gmail.com>
2016-08-05 03:35:59 +0300

    Массивы

---
+++ homework6.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ingvar73
+ * Date: 05.08.2016
+ * Time: 2:04
+ */
+    $cars = array
+    (
+        'BMW' => array('model' => "X5", 'speed' => 120, 'doors' => 5, 'year' => "2015"),
+        'TOYOTA' => array('model' => "Camry", 'speed' => 140, 'doors' => 4, 'year' => "2013"),
+        'OPEL' => array('model' => "Astra", 'speed' => 130, 'doors' => 4, 'year' => "2014")
+    ); // Объявляем многомерный массив
+    foreach ($cars as $key => $kind)
+    {
+        echo "CAR ".$key."<br>";
+        foreach ($kind as $value)
+        {
+            echo $value, '<br>';
+        }
+    }
+
