5422649c541e4d9183b369e01164777d49cc82ee
ingvar73 <ingvar73@gmail.com>
2016-08-10 22:05:44 +0300

    LC

--- dz2/exercise2.php
+++ dz2/exercise2.php
@@ -5,12 +5,12 @@
  * Date: 10.08.2016
  * Time: 13:44
  */
-    $arr_num = array(60, 2, 3, 4, 5, 6, 1, 7, 8, 0, 10);
-    arr_disp($arr_num, '/');
+    $arr_num = array(60, 2, 3, 4, 5, 6, 1, 7, 0, 23, 10);
+    arr_disp($arr_num, '+');
 
     function arr_disp ($p1, $p2){
 
-        if (is_array($p1) || is_numeric($p1)) {
+        if (!is_numeric($p1)) {
             switch ($p2) {
                 case '-':
                     $res = reset($p1);
@@ -42,7 +42,7 @@
                 case '/':
                     $res = reset($p1);
                     for ($i = 0; $i < count($p1); $i++) {
-                        if (isset($p1[$i]) === 0 && !is_numeric($p1)) {
+                        if ($p1[$i] === 0 || isset($p1[$i])) {
                             exit('Деление на 0!');
                         } else {
                             $res /= $p1[$i];
