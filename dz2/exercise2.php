0f789bd857d6bc16f28646cd36da832a1811c94d
ingvar73 <ingvar73@gmail.com>
2016-08-10 22:21:40 +0300

    Исправление

--- dz2/exercise2.php
+++ dz2/exercise2.php
@@ -6,41 +6,38 @@
  * Time: 13:44
  */
     $arr_num = array(60, 2, 3, 4, 5, 6, 1, 7, 0, 23, 10);
-    arr_disp($arr_num, '+');
+    arr_disp($arr_num, '-');
 
     function arr_disp ($p1, $p2){
 
         if (!is_numeric($p1)) {
             switch ($p2) {
                 case '-':
-                    $res = reset($p1);
+                    $res = 0;
                     for ($i = 0; $i < count($p1); $i++) {
                         $res -= $p1[$i];
                     }
-                    echo "Результат вычитания всех " . count($p1) . " элементов массива - ";
-                    echo $res;
+                    echo "Результат вычитания всех " . count($p1) . " элементов массива - ".$res;
                     break;
 
                 case '+':
-                    $res = reset($p1);
+                    $res = 0;
                     for ($i = 0; $i < count($p1); $i++) {
                         $res += $p1[$i];
                     }
-                    echo "Результат сложения всех " . count($p1) . " элементов массива - ";
-                    echo $res;
+                    echo "Результат сложения всех " . count($p1) . " элементов массива - ".$res;
                     break;
 
                 case '*':
-                    $res = reset($p1);
+                    $res = 0;
                     for ($i = 0; $i < count($p1); $i++) {
                         $res *= $p1[$i];
                     }
-                    echo "Результат умножения всех " . count($p1) . " элементов массива - ";
-                    echo $res;
+                    echo "Результат умножения всех " . count($p1) . " элементов массива - ".$res;
                     break;
 
                 case '/':
-                    $res = reset($p1);
+                    $res = 0;
                     for ($i = 0; $i < count($p1); $i++) {
                         if ($p1[$i] === 0 || isset($p1[$i])) {
                             exit('Деление на 0!');
@@ -48,8 +45,10 @@
                             $res /= $p1[$i];
                         }
                     }
-                    echo "Результат деления всех " . count($p1) . " элементов массива - ";
-                    echo $res;
+                    echo "Результат деления всех " . count($p1) . " элементов массива - ".$res;
+                    break;
+                default:
+                    echo "Ввели некорректное арифметическое действие";
                     break;
             }
         } else {
