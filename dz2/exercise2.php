981ef45781e7c3799c8dbf0adbef43558589b37b
Igor Galochkin <ingvar73@gmail.com>
2016-08-10 15:45:23 +0300

    first commit

--- dz2/exercise2.php
+++ dz2/exercise2.php
@@ -5,42 +5,46 @@
  * Date: 10.08.2016
  * Time: 13:44
  */
-    $arr_num = array(3, 6, 6, 8, 12, 23, 0, 454, 667);
-    arr_disp($arr_num, '/');
+    $arr_num = array(1, 2, 3, 4, 5, 6, 1, 7, "строка", array());
+    arr_disp($arr_num, '-');
     function arr_disp ($p1, $p2){
         switch ($p2){
             case '-':
-                echo "Результат вычитания всех ".count($p1)." элементов массива - ";
                 $res = reset($p1);
                 for ($i=0; $i<count($p1); $i++) {
-                    $res -= $p1[$i];
+                    if (is_numeric($p1[$i])){
+                    $res -= $p1[$i];}
+                    elseif (is_array($p1[$i])) {
+                        echo "Не число";}
+                    break;
                 }
+                echo "Результат вычитания всех ".count($p1)." элементов массива - ";
                 echo $res;
                 break;
             case '+':
-                echo "Результат сложения всех ".count($p1)." элементов массива - ";
                 $res = reset($p1);
                 for ($i=0; $i<count($p1); $i++) {
                     $res += $p1[$i];
                 }
+                echo "Результат сложения всех ".count($p1)." элементов массива - ";
                 echo $res;
                 break;
             case '*':
-                echo "Результат умножения всех ".count($p1)." элементов массива - ";
                 $res = reset($p1);
                 for ($i=0; $i<count($p1); $i++) {
                     $res *= $p1[$i];
                 }
+                echo "Результат умножения всех ".count($p1)." элементов массива - ";
                 echo $res;
                 break;
             case '/':
-                echo "Результат деления всех ".count($p1)." элементов массива - ";
                 $res = reset($p1);
                 for ($i=0; $i<count($p1); $i++) {
-                    if ($p1[$i] == 0)
+                    if (!$p1[$i] == 0)
                     $res /= $p1[$i];
-                    break;
+                    else echo "Возможно деление на 0! ";
                 }
+                echo "Результат деления всех ".count($p1)." элементов массива - ";
                 echo $res;
                 break;
         }
