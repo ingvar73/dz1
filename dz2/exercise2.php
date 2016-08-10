c8bf58408ed5ecb88e28012ebbba824169f77b2c
ingvar73 <ingvar73@gmail.com>
2016-08-10 23:03:16 +0300

    Исправление

--- dz2/exercise2.php
+++ dz2/exercise2.php
@@ -5,12 +5,12 @@
  * Date: 10.08.2016
  * Time: 13:44
  */
-    $arr_num = array(6, 2, 3, 4, 5, 6, 1, 0, 2, 3, 10);
-    arr_disp($arr_num, '-');
+    $arr_num = array(6, 2, 3, 4, 5, 6, 1, 2, "fhfhfgh", 3, 10);
+    arr_disp($arr_num, '/');
 
     function arr_disp ($p1, $p2){
 
-        if (!is_numeric($p1)) {
+        if (isset($p1)) {
             switch ($p2) {
                 case '-':
                     $res = 0;
@@ -36,7 +36,7 @@
                 case '/':
                     $res = 1;
                     for ($i = 0; $i < count($p1); $i++) {
-                        if ($p1[$i] === 0 || !isset($p1[$i])) {
+                        if (($p1[$i] == 0) || !isset($p1[$i])) {
                             exit('Деление на 0!');
                         } else {
                             $res /= $p1[$i];
@@ -49,6 +49,7 @@
             }
         } else {
             echo "Массив содержит недопустимые символы или не является числовым!";
+            exit;
         }
         echo "Результат для \" {$p2} \" " . count($p1) . " элементов массива - ".$res;
     }
