4836f118c491e37b51ae70296b0e485bac88e432
ingvar73 <ingvar73@gmail.com>
2016-08-13 12:00:30 +0300

    packets

--- dz2/exercise7-2.php
+++ dz2/exercise7-2.php
@@ -6,15 +6,16 @@
  * Time: 22:26
  */
 function smile(){
-    echo "<br>";
     echo "¯\_(ツ)_/¯";
     echo "<br>";
 }
 
-$number='Мой номер сотового: +7 916-901-33-11';
+$number='Мой номер сотового: +7 916-905-37-18';
 if (!preg_match("/(\+7\s|8\s)([\d|\s|-]{13})/",$number, $matches))
     echo 'В строке '.$number.' не найден российский номер мобильного!';
     else echo 'Найден номер '.$matches[0];
+    echo "<br>";
+    echo "<hr>";
     echo "<br>";
 
 function rx_packets ($string) {
@@ -24,7 +25,7 @@ function rx_packets ($string) {
         smile();
     } else {
         // Проверяем на количество пакетов
-        preg_match("#(packets.*?)\s#i", $string, $result_temp);
+        preg_match("#(RX.*?)\s#i", $string, $result_temp);
         preg_match("#\d.*#", $result_temp[0], $result);
         if ((int)$result[0] > 1000) {
             echo "Сеть есть";
@@ -33,4 +34,4 @@ function rx_packets ($string) {
         }
     }
 }
-rx_packets ("RX packets:10011 errors:0 dropped:0 overruns:0 frame:0. ");
+rx_packets ("RX packets:10011 errors:0 :) dropped:0 overruns:0 frame:0. ");
