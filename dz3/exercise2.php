139d665f665967bfb9efed88d0ce562178db0394
Igor Galochkin <ingvar73@gmail.com>
2016-08-17 16:41:32 +0300

    update

--- dz3/exercise2.php
+++ dz3/exercise2.php
@@ -15,8 +15,8 @@
 $diff_array = [
     "Компьютерные комплектующие" => [
         "Процессоры" => ["Intel" => [
-            "Core i7-6950X" => "141 900 rub", "Core i7-6900K" => "89 990 rub"],
-            "AMD" => ["A10-7890K" => "10 380 rub", "A10-7860K" => "8 390 rub"]],
+            "Core i7-6950X" => "141900", "Core i7-6900K" => "89990"],
+            "AMD" => ["A10-7890K" => "10380", "A10-7860K" => "8390"]],
         "Материнские платы" => ["ASUS", "ASRock", "MSI", "Gigabyte"],
         "Видеокарты" => ["NVidia", "Radeon"]],
     "Ноутбуки" => ["Ультрабуки", "MacBook", "Трансформеры", "Игровые"]
@@ -25,3 +25,29 @@ $diff_array = [
 echo "<pre>";
 print_r($diff_array);
 
+function arr_to_json($array){
+    $fp = fopen('output.json', 'w');
+    fwrite($fp, json_encode($array, JSON_UNESCAPED_UNICODE));
+    fclose($fp);
+}
+
+arr_to_json($diff_array);
+
+function change_json(){
+    $arr = json_decode(file_get_contents('output.json'), true);
+    foreach ($arr as &$arr_2){
+        foreach ($arr_2 as &$value){
+            foreach ($value as $val){
+                if (is_numeric($val)){
+                    $val *= 0.9;
+                }
+            }
+            unset ($value);
+        }
+    }
+    $fp = fopen('output2.json', 'w');
+    fwrite($fp, json_encode($arr, JSON_UNESCAPED_UNICODE));
+    fclose($fp);
+}
+
+change_json();
