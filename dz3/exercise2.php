628b118515377831f50b37fa4962e468d3a446b9
ingvar73 <ingvar73@gmail.com>
2016-08-17 20:21:27 +0300

    update

--- dz3/exercise2.php
+++ dz3/exercise2.php
@@ -18,8 +18,7 @@ $diff_array = [
             "Intel" => ["Core i7-6950X" => 141900, "Core i7-6900K" => 89990],
             "AMD" => ["A10-7890K" => 10380, "A10-7860K" => 8390]],
         "Материнские платы" => ["ASUS", "ASRock", "MSI", "Gigabyte"],
-        "Видеокарты" => ["NVidia", "Radeon"]],
-    "Ноутбуки" => ["Ультрабуки", "MacBook", "Трансформеры", "Игровые"]
+        "Видеокарты" => ["NVidia", "Radeon"]]
 ];
 
 //echo "<pre>";
@@ -35,8 +34,8 @@ arr_to_json($diff_array);
 
 function change_json(){
     $arr = json_decode(file_get_contents('output.json'), true);
-    foreach ($arr as $key => $arr_2){
-        foreach ($arr_2 as $key1 => $value){
+    foreach ($arr as $key => &$arr_2){
+        foreach ($arr_2 as $key1 => &$value){
            //echo $key1."\n";
             foreach ($value as $key2 => &$val){
                 if (is_array($val)) {
@@ -45,8 +44,7 @@ function change_json(){
                     //print_r ($val);
                     foreach ($val as $key3 => &$res) {
                         if (is_numeric($res)) {
-                            $res *= 0.9;
-                            echo $res;
+                            $res *= 0.9; //Скидка в 10% на процессоры
                         }
                     }
                 }
@@ -60,3 +58,15 @@ function change_json(){
 }
 
 change_json();
+
+function get_different(){
+    $array_1 = json_decode(file_get_contents('output.json'), true);
+    $array_2 = json_decode(file_get_contents('output2.json'), true);
+
+    echo "Элементы файла output.json которых нет в файле output2.json<br>";
+    print_r(array_diff($array_1["Компьютерные комплектующие"]["Процессоры"]["Intel"], $array_2["Компьютерные комплектующие"]["Процессоры"]["Intel"]));
+    echo "<br />";
+    print_r(array_diff($array_1["Компьютерные комплектующие"]["Процессоры"]["AMD"], $array_2["Компьютерные комплектующие"]["Процессоры"]["AMD"]));
+}
+
+get_different();
