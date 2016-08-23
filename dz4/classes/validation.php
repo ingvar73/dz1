c8d5cce51308c25c25ccb1f019f7e00fa15faadc
ingvar73 <ingvar73@gmail.com>
2016-08-23 03:26:42 +0300

    update

--- dz4/classes/validation.php
+++ dz4/classes/validation.php
@@ -24,7 +24,6 @@ private $pass1;
         $result_age = $this->ver_age($age);
         $result_about = $this->ver_about($about);
         $result_pass = $this->ver_pass($pass, $pass1);
-//        $result_pass1 = $this->ver_pass1($pass1);
         if($result_login and $result_name and $result_age and $result_about and $result_pass){
             return true;
         } else{
@@ -34,7 +33,7 @@ private $pass1;
         public function ver_login($login){
             $this->login = strip_tags(trim($login));
             if(strlen(htmlspecialchars($this->login)) < 6 || strlen(htmlspecialchars($this->login)) > 15){
-                echo "ОШИБКА! Длина логина должна быть не менее 6 и не более 15 символов!";
+                echo "ОШИБКА! Длина логина должна быть не менее 6 и не более 15 символов!\n";
                 return false;
             }
             return true;
@@ -43,7 +42,7 @@ private $pass1;
         public function ver_name($name){
             $this->name = strip_tags(trim($name));
             if(strlen(htmlspecialchars($this->name)) < 6 || strlen(htmlspecialchars($this->name)) > 15){
-                echo "ОШИБКА! Длина логина должна быть не менее 6 и не более 15 символов!";
+                echo "ОШИБКА! Длина логина должна быть не менее 6 и не более 15 символов!\n";
                 return false;
             }
             return true;
@@ -52,7 +51,7 @@ private $pass1;
         public function ver_age($age){
             $this->age = (int)$age;
             if (is_numeric($this->age) && $this->age === 0){
-                echo "В поле ввода возраста введено не числовое значение!";
+                echo "В поле ввода возраста введено не числовое значение!\n";
                 return false;
             } else
             return true;
@@ -61,10 +60,10 @@ private $pass1;
         public function ver_about($about){
             $this->about = strip_tags($about);
             if(strlen(nl2br(htmlspecialchars($this->about))) == ''){
-                echo "ОШИБКА! Вы не ввели описание!";
+                echo "ОШИБКА! Вы не ввели описание!\n";
                 return false;
             } elseif (strlen(nl2br(htmlspecialchars($this->about))) > 300){
-                echo "ОШИБКА! Превышено максимальное количество символов для описания!";
+                echo "ОШИБКА! Превышено максимальное количество символов для описания!\n";
                 return false;
             }
             return true;
@@ -75,23 +74,9 @@ private $pass1;
             $this->pass1 = trim($pass1);
             if((strlen(nl2br(htmlspecialchars($this->pass))) == null) && (strlen(nl2br(htmlspecialchars($this->pass1))) == null)) echo "Не ввели пароль!";
             if($this->pass != $this->pass1) {
-                echo "Пароли не совпадают";
+                echo "Пароли не совпадают\n";
                 return false;
             }
             return true;
         }
-
-//        function ver_pass1($pass1){
-//            $this->pass1 = $pass1;
-//            if($this->pass1 == '') "Не ввели пароль!";
-//            return true;
-//        }
-//
-//        function comparing (){
-//            if($this->pass != $this->pass1){
-//                echo "Повторный пароль не совпадает!";
-//                return $result = false;
-//            } else
-//                return $result = true;
-//        }
 }
