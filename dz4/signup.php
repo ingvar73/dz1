9278438988261843c288914a6323f4a3785e04c7
Igor Galochkin <ingvar73@gmail.com>
2016-08-24 15:48:21 +0300

    Last update

--- dz4/signup.php
+++ dz4/signup.php
@@ -24,7 +24,7 @@ if ($data->result == true){
         // регистрируем
     $sql = "INSERT INTO users VALUES (?, ?, ?, ?, ?)";
     if ($stmt = $mysql->prepare($sql)) {
-        $stmt->bind_param('si', $login, $name, $age, $about, $pass);
+        $stmt->bind_param('ssiss', $login, $name, $age, $about, $pass);
         $stmt->execute();
         $stmt->close();
         $mysql->close();
@@ -68,22 +68,22 @@ if ($data->result == true){
                         <form method=POST action="signup.php">
                             <div class="form-inline">
                                 <input type="text" name="login" class="form-control" placeholder="Login" value="<?php echo @$fdata['login'];?>">
-                            </div>
+                            </div><br />
                             <div class="form-inline">
                                 <input type="text" name="name" class="form-control" placeholder="Имя" value="<?php echo @$fdata['name'];?>">
-                            </div>
+                            </div><br />
                             <div class="form-inline">
                                 <input type="text" name="age" class="form-control" placeholder="Ваш возраст" value="<?php echo @$fdata['age'];?>">
-                            </div>
+                            </div><br />
                             <div class="form-inline">
                                 <textarea rows="10" name="about" cols="23" placeholder="О себе" value="<?php echo @$fdata['about'];?>"></textarea>
-                            </div>
+                            </div><br />
                             <div class="form-inline">
                                 <input type="password" name="password" class="form-control" placeholder="Пароль">
-                            </div>
+                            </div><br />
                             <div class="form-inline">
                                 <input type="password" name="password1" class="form-control" placeholder="Повторите пароль">
-                            </div>
+                            </div><br />
 <!--                            <div class="form-inline">-->
 <!--                                <label for="exampleInputFile">Загрузить изображение</label>-->
 <!--                                <input type="file" id="avatar">-->
